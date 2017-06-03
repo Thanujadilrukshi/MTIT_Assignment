9a6f5ac7d174c7676627e8fae224fb791224efc1
Thanujadilrukshi <liyanapathiranagethanuja@gmail.com>
2017-06-03 15:33:48 +0530

    Create  a game frame

---
+++ app/src/main/java/com/example/user/newgame/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.newgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 =(Button) findViewById(R.id.b1);
+        b2 =(Button) findViewById(R.id.b2);
+        b3 =(Button) findViewById(R.id.b3);
+        b4 =(Button) findViewById(R.id.b4);
+        b5 =(Button) findViewById(R.id.b5);
+        b6 =(Button) findViewById(R.id.b6);
+        b7 =(Button) findViewById(R.id.b7);
+        b8 =(Button) findViewById(R.id.b8);
+        b9 =(Button) findViewById(R.id.b9);
+
+        
+    }
+}
