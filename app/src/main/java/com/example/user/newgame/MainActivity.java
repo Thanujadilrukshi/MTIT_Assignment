467f2aef82e26cb48a81d241d7d988cde83eef87
Thanujadilrukshi <liyanapathiranagethanuja@gmail.com>
2017-06-04 18:23:13 +0530

    Add a loop

--- app/src/main/java/com/example/user/newgame/MainActivity.java
+++ app/src/main/java/com/example/user/newgame/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.newgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,151 @@ public class MainActivity extends AppCompatActivity {
         b8 =(Button) findViewById(R.id.b8);
         b9 =(Button) findViewById(R.id.b9);
 
-        
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b1.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b1.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b1.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b2.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b2.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b2.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b3.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b3.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b3.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b4.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b4.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b4.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b5.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b5.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b5.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b6.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b6.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b6.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b7.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b7.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b7.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b8.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b8.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b8.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b9.getText().toString().equals("")) {
+                    if (turn ==1){
+                        turn =2;
+                        b9.setText("X");
+                    } else  if (turn == 2) {
+                        turn =1 ;
+                        b9.setText("O");
+                    }
+                }
+                endGame();
+            }
+        });
+
     }
 }
